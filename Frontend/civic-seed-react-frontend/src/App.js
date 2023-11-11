37905b84a10bf14bd24cd0aba973f36f762b53e4
Samir <ssh325@lehigh.edu>
2023-11-11 16:49:47 -0500

    Created routes

--- Frontend/civic-seed-react-frontend/src/App.js
+++ Frontend/civic-seed-react-frontend/src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import React from "react";
+//import logo from './logo.svg'; //will use this later
+import "./App.css";
+import LoginPage from "./Resident/LoginPage/LoginPageResident"; // Import the LoginPage component
+import IdeaPage from "./Resident/IdeaPage/IdeaPageResident"; // Import the LoginPage component
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Civic Seed</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/ideas" element={<IdeaPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
