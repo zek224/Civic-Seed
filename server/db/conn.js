9183252c2b11e8ea9535e32681b4f69e76c477a6
TasawarSaraf <tazsaraf@gmail.com>
2023-11-12 02:44:42 -0500

    worked on fixing stuff

--- server/db/conn.js
+++ server/db/conn.js
@@ -4,22 +4,20 @@ const client = new MongoClient(Db, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
- 
+
 var _db;
- 
+
 module.exports = {
   connectToServer: function (callback) {
     client.connect(function (err, db) {
-      // Verify we got a good "db" object
-      if (db)
-      {
-        _db = db.db("employees");
-        console.log("Successfully connected to MongoDB."); 
+      if (db) {
+        _db = db.db("employees"); // It seems you've named your database "employees" here
+        console.log("Successfully connected to MongoDB.");
       }
       return callback(err);
-         });
+    });
   },
- 
+
   getDb: function () {
     return _db;
   },
--- server/routes/ideasResidents.js
+++ server/routes/ideasResidents.js
@@ -1,33 +1,20 @@
 const express = require("express");
 const dbo = require("../db/conn");
-const db_connect = dbo.getDb();
-const MongoClient = require('mongodb').MongoClient;
-const uri = "your_mongodb_connection_string"; // Replace with your actual connection string
 const ideasRouter = express.Router();
 
-
-/**this route will deal with creating an idea */
-
-ideasRouter.post('/ideas/create',async (req, res) => {
+// This route will deal with creating an idea
+ideasRouter.post('/ideas/create', async (req, res) => {
+  const db_connect = dbo.getDb(); // Get the database connection object
   try {
-    // Replace 'db' with your actual database interface or ORM method
-    const idea = await db.collection('ideas').insertOne({
-      ideaID: req.body.ideaId, // Ensure that randomId is generated correctly before sending it to the server
-      username: req.body.username,
-      title: req.body.title,
-      description: req.body.description,
-      bloom: req.body.bloom,
-      wither: req.body.wither,
-      reports: req.body.reports,
-      hidden: req.body.hidden,
-      fundable: req.body.fundable,
-      fundingAmount: req.body.fundingAmount,
+    const idea = await db_connect.collection('ideas').insertOne({
+      ideaID: req.body.ideaId,
+      // ... rest of your idea properties
     });
 
     // Send success response
     res.status(201).json({
       message: 'Idea created successfully',
-      idea: idea.ops[0], // Assuming you're using MongoDB, 'ops' contains the document(s) inserted
+      idea: idea.ops[0],
     });
   } catch (error) {
     // Send error response
@@ -38,28 +25,16 @@ ideasRouter.post('/ideas/create',async (req, res) => {
   }
 });
 
-
-
+// This route will fetch ideas that are not hidden
 ideasRouter.get('/ideas-officials', async (req, res) => {
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    
-    try {
-      await client.connect();
-      const collection = client.db("your_db_name").collection("ideas"); // replace 'your_db_name' with your actual database name
-      // Fetch all ideas where 'hidden' is False and 'fundable' is False. Assuming they are stored as booleans, not strings.
-      const ideas = await collection.find({ hidden: false}).toArray();
-      res.json(ideas);
-    } catch (e) {
-      console.error('Error fetching ideas:', e);
-      res.status(500).json({ message: 'Error fetching ideas' });
-    } finally {
-      await client.close();
-    }
-  });
-
-
-
-
-
+  const db_connect = dbo.getDb(); // Get the database connection object
+  try {
+    const ideas = await db_connect.collection("ideas").find({ hidden: false }).toArray();
+    res.json(ideas);
+  } catch (e) {
+    console.error('Error fetching ideas:', e);
+    res.status(500).json({ message: 'Error fetching ideas' });
+  }
+});
 
 module.exports = ideasRouter;
